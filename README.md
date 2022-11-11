7fc1420c333d50e85dc1b52806ff09166745a7db
Kunaphas.Kong <kunaphas.kon@gmail.com>
2022-11-11 22:02:04 +0100

    add mpxsonar subtree

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # MPXRadar-frontend
 MonkeyPoxRadar frontend codebase.
+
+# Installation
+
+´´´
+´´´
+
+
+----
+
+# Contribution
+
+Always check code version from dev branch before coding.
+´´´
+git checkout dev
+git pull
+´´´
+
+## Install dependencies
+
+´´´
+conda create -n mpxsonar-dev python=3.9 poetry fortran-compiler nox pre-commit emboss=6.6.0
+conda activate covsonar-dev
+git config blame.ignoreRevsFile .git-blame-ignore-revs  # ignore black reformatting when doing git blame
+pre-commit install  # install pre-commit hooks for formatting and linting
+poetry install  # install current source of covsonar and its dependencies
+´´´
+
+## Before Commit/Push
+1. Check current branch.
+2. Run python format checking -> `pre-commit run --all-files`, Fix it if found, otherwise you will not be allowed to commit and push.
+
+# Add/Update MPXsonar
+
+
+## First time add MPXsonar
+´´´
+git subtree add --prefix libs/mpxsonar https://github.com/silenus092/mpxsonar dev --squash
+´´´
+
+## Update MPXsonar
+´´´
+git subtree pull --prefix libs/mpxsonar https://github.com/silenus092/mpxsonar dev --squash
+´´´
---
+++ example.app.py
@@ -0,0 +1,155 @@
+import argparse
+
+from app_controller import get_freq_mutation
+from app_controller import match_controller
+from app_controller import sonarBasicsChild
+from dash import Dash
+from dash import dash_table
+from dash import dcc
+from dash import html
+from dash import Input
+from dash import Output
+from dash import State
+import dash_bootstrap_components as dbc
+from libs.mpxsonar.src.mpxsonar.sonar import parse_args
+
+# stylesheet with the .dbc class
+dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP, dbc_css])
+custom_cmd_cards = html.Div(
+    [
+        dbc.Card(
+            [
+                dbc.CardHeader([html.H3("MPXsonar command!")]),
+                dbc.CardBody(
+                    dbc.Row(
+                        [
+                            dbc.Col(
+                                [
+                                    html.Div(
+                                        [
+                                            "sonar: ",
+                                            dcc.Input(
+                                                id="my-input",
+                                                value="match --count",
+                                                type="text",
+                                                size="100",
+                                            ),
+                                        ]
+                                    ),
+                                    html.Br(),
+                                    html.Label("MPXSonar command:"),
+                                    html.Div(id="my-command", children=""),
+                                ]
+                            ),
+                            dbc.Col(
+                                [
+                                    html.Div(
+                                        dbc.Button(
+                                            id="submit-button-state",
+                                            n_clicks=0,
+                                            children="Submit",
+                                            color="primary",
+                                            className="mb-2",
+                                        ),
+                                    ),
+                                ]
+                            ),
+                        ]
+                    ),
+                ),
+            ],
+            className="mb-3",
+        ),
+        dbc.Card(
+            [
+                html.Label("Output:"),
+                html.Div(id="my-output", children=""),
+                html.Div(
+                    [
+                        dash_table.DataTable(
+                            id="my-output-df",
+                            page_current=0,
+                            page_size=10,
+                            style_data={
+                                "whiteSpace": "normal",
+                                "height": "auto",
+                            },
+                            style_table={"overflowX": "auto"},
+                            export_format="csv",
+                        ),
+                    ]
+                ),
+            ],
+            body=True,
+        ),
+    ]
+)
+
+app.layout = dbc.Container(
+    [
+        dbc.Row([]),
+        html.Hr(),
+        dbc.Row(
+            [
+                custom_cmd_cards,
+                html.Br(),
+            ]
+        ),
+    ]
+)
+
+
+@app.callback(
+    Output(component_id="my-command", component_property="children"),
+    Input(component_id="my-input", component_property="value"),
+)
+def update_output_div(input_value):
+    return f"sonar {input_value}"
+
+
+@app.callback(
+    Output(component_id="my-output", component_property="children"),
+    Output(component_id="my-output-df", component_property="data"),
+    Output(component_id="my-output-df", component_property="columns"),
+    Input("submit-button-state", "n_clicks"),
+    State("my-input", "value"),
+)
+def update_output_sonar(n_clicks, commands):
+    # calls backend
+    _list = commands.split()
+    print(_list)
+    # need to implement mini parser
+    data = None
+    columns = None
+    try:
+        args = parse_args(_list)
+        output = ""
+        if args.tool == "list-prop":
+            df = sonarBasicsChild.list_prop()
+            columns = [{"name": col, "id": col} for col in df.columns]
+            data = df.to_dict(orient="records")
+        elif args.tool == "match":
+            _tmp_output = match_controller(args)
+            if type(_tmp_output) == int:
+                output = _tmp_output
+            elif type(_tmp_output) == str:
+                output = _tmp_output
+            else:
+                df = _tmp_output
+                columns = [{"name": col, "id": col} for col in df.columns]
+                data = df.to_dict(orient="records")
+        elif args.tool == "dev":
+            get_freq_mutation(args)
+        else:
+
+            output = "This command is not available."
+    except argparse.ArgumentError as exc:
+        output = exc.message
+    except SystemExit:
+        output = "error: unrecognized arguments/commands"
+    return output, data, columns
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True, host="127.0.0.1")
